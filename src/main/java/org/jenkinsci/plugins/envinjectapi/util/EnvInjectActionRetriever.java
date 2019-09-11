67a0e3bc40e410825ced48fd6114e69ea8b1b487
Stefan Spieker <S.Spieker@gmx.net>
2019-09-11 20:15:21 +0200

    reverted the getEnvInjectAction() change

--- src/main/java/org/jenkinsci/plugins/envinjectapi/util/EnvInjectActionRetriever.java
+++ src/main/java/org/jenkinsci/plugins/envinjectapi/util/EnvInjectActionRetriever.java
@@ -51,7 +51,7 @@ public class EnvInjectActionRetriever {
             LOGGER.log(Level.WARNING, "There was a problem in the invocation of getParentBuild in hudson.matrix.MatrixRun", e);
         }
 
-        List<? extends Action> actions = run.getAllActions();
+        List<Action> actions = run.getActions();
         for (Action action : actions) {
             if (action == null) {
                 continue;
